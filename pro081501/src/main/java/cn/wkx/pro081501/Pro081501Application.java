7616232ece92bfa3fae2d25ca997811f40efd5f6
wukangxin1 <320669425@qq.com>
2022-08-15 08:02:36 +0800

    Initial Commit

---
+++ pro081501/src/main/java/cn/wkx/pro081501/Pro081501Application.java
@@ -0,0 +1,13 @@
+package cn.wkx.pro081501;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro081501Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pro081501Application.class, args);
+	}
+
+}
---
+++ pro081501/src/test/java/cn/wkx/pro081501/Pro081501ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wkx.pro081501;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pro081501ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
